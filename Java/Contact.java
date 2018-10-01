e2dcf0e60c21bd78526da68d1c7ed3a18a8a94a7
Virginie C <34030149+JavaVivi@users.noreply.github.com>
2018-10-01 19:05:08 +0200

    Update Contact.java

--- Java/Contact.java
+++ Java/Contact.java
@@ -3,3 +3,32 @@ package td3.contact;
 //author : vcaze L2
 
 public class Contact {
+  private String nom;
+  private String numero;
+  private String mail;
+  
+  public Contact() {
+    this("Virginie", "06666666", "andromede@gmail.com");
+  }
+  
+  public Contact(String nom, String numero, String mail) {
+    this.nom = nom;
+    this.numero;
+    this.mail = mail;
+  }
+  
+  
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+ }      
