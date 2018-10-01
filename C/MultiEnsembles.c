8462b4c5f8612a771ce743a1f558d667393986b5
Virginie C <34030149+JavaVivi@users.noreply.github.com>
2018-10-01 18:52:09 +0200

    Update MultiEnsembles.c

--- C/MultiEnsembles.c
+++ C/MultiEnsembles.c
@@ -1,2 +1,66 @@
 //suite et fin du TD/TP 1 & 2 d'Algo
 
+int SuppVal ( MultiEnsemble me, int valeur ) {
+  int i, j;
+  for (i = 0; i < me.NbElem; i++) {
+    if ( me.tab[i].val == valeur ) {
+          me.tab(i].nbOcc--;
+     }
+		 if ( me.tab[i].nbOcc == 0) {
+			 j = i;
+			 while ( j < me.NbElem ) {
+				 me.tab(j] = me.tab(j+1];
+				 j++;
+			 }
+				me.tab(j]=NULL;
+				me.NbElem--;
+				return me;
+			}
+		else if ( me.tab(i].val > valeur) {
+			return me;
+		}
+	 }
+	}
+
+int Union ( MultiEnsemble m1, MultiEnsemble m2) {
+    MultiEnsemble m3;
+    int i;
+    int k = 1;
+    int j = 1;
+    for (i = 0; i < m1.NbElem; i++) {
+        if(j <= m2.nbElem) {
+            while(m1.tab(i].val > m2.tab(j].val) {
+                m3.tab(k] = m2.tab[j];
+                j++;
+                k++;
+                m3.NbElem++;
+            }
+            if(m1.tab[i].val < m2.tab[j].val) {
+                m3.tab[k] = m1.tab[i];
+                k++;
+                m3.NbElem++;
+            }else {
+                m3.tab[k].val = m1.tab[i].val;
+                m3.tab[k].nbOcc = m1.tab[i].nbOcc;
+                j++;
+                k++;
+                m3.nbElem;
+            }
+       else { 
+           m3.tab[k] = m1.tab[i];
+           k++;
+           m3.NbElem++;
+    }
+     while ( j =< m2.NbElem) {
+         m3.tab[k] = m2.tab[j];
+         k++;
+         j++;
+         m3.nbElem;
+     }
+      return m3;
+ }                       
+						 
+				
+		
+                 
+      
