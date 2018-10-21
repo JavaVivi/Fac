639b8183b1d22f94b1a3bb3e30f7de532867c18b
Virginie C <34030149+JavaVivi@users.noreply.github.com>
2018-10-21 11:55:02 +0200

    Update ListeChainees.c

--- C/ListeChainees.c
+++ C/ListeChainees.c
@@ -1,1 +1,21 @@
-test
+//suite et fin du td3 
+
+Bool existVal(int e, Liste *l) {
+  bool i = FALSE;
+  if(TestVide(*l) == 0) {
+    return FALSE;
+  }
+  else {
+    while(*l != NULL) {
+      if(e == (*l).val) {
+        i = TRUE;
+        return i;
+      }
+      *l = l->suivant;
+    }
+    return i;
+  }
+}
+
+
+  
