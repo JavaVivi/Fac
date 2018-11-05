93e544abf45aac89144946a24a21786a27edfc3a
Virginie C <34030149+JavaVivi@users.noreply.github.com>
2018-11-05 18:40:06 +0100

    Update Polynomes.c

--- C/Polynomes.c
+++ C/Polynomes.c
@@ -1,1 +1,30 @@
 //suite et fin du td sur les Polynomes 
+
+Polynome suppTete ( Polynome *p ) {
+  Polynome *tmp = NULL;
+  Polynome *courant = *p;
+  while(*courant != NULL) {
+    *tmp = courant->suivant;
+    free(*courant);
+    *courant = *tmp;
+  }
+  return *p;
+}
+
+Monome appartientMonome (Monome *m, Polynome *p) {
+  if(videPoly(*p) == 1) {
+    return NULL;
+  } else {
+    while(*p != NULL) {
+      if(p->membre == m) {
+        return p->membre;
+      }
+      *p = p->suivant;
+    }
+    return NULL;
+  }
+}
+
+
+  
+  
